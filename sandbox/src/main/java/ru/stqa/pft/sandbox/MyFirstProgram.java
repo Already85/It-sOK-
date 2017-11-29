ba9496073cffc2d120021911e10333b172f2c2ad
LinkyJax <Kaledo2805@gmail.com>
2017-11-29 13:34:57 +0300

    Решение задачи при помощи функции

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,5 +1,14 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
-	public static void main(String[] args) { System.out.println("Hello, world!"); }
+
+ public static void main(String[] args) {
+   Point p1 = new Point(4.0,2.0);
+   Point p2 = new Point(8,4);
+   System.out.println("Расстояние между двумя точками равно " + distance(p1,p2));
+ }
+public static double distance(Point p1, Point p2) {
+     return Math.sqrt(Math.pow(p2.x2-p1.x1, 2) + Math.pow(p2.y2-p1.y1, 2) );
+   }
+
 }
