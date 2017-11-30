9a9a5b1e1fbde7feaaba226eed1796c520a77721
LinkyJax <Kaledo2805@gmail.com>
2017-11-30 18:27:03 +0300

    Решение задачи 1,1.

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,7 +2,7 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
   public static void main(String[] args) {
-    Point p1 = new Point(4.0, 2.0);
+    Point p1 = new Point(5.0, 2.0);
     Point p2 = new Point(8, 4);
     System.out.println("Расстояние между двумя точками равно " + p1.distance(p2));
   }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,21 +1,20 @@
 package ru.stqa.pft.sandbox;
 
-public class Point {
+  public class Point {
   public double x;
   public double y;
 
-  public Point(double x, double y) {
-    this.x = x;
-    this.y = y;
+  public Point(double x1, double y1) {
+    this.x = x1;
+    this.y = y1;
   }
 
-  public Point(int x, int y ) {
-    this.x = x;
-    this.y = y;
+  public Point(int x2, int y2 ) {
+    x = x2;
+    y = y2;
   }
+
   public double distance(Point p2) {
-    return Math.sqrt(Math.pow(this.x - this.x, 2) + Math.pow(this.y - this.y, 2));
+    return Math.sqrt(Math.pow(p2.x - this.x, 2) + Math.pow(p2.y - this.y, 2));
   }
 }
-
-
