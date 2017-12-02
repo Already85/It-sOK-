12ab42cadd0e9829f332c09e58079e08252e6a5d
LinkyJax <Kaledo2805@gmail.com>
2017-12-02 18:33:23 +0300

    Решение задания 1.2.

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/DistanceTests.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DistanceTests {
+
+  @Test
+public void testDistance() {
+
+  Point p1 = new Point(5.0, 2.0);
+  Point p2 = new Point(8, 4);
+    Assert.assertEquals(p1.distance(p2), 3.605551275463989);
+}
+}
