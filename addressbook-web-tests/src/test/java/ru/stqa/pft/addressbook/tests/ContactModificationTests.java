b0f0a6223a4bcad7e181274dc3035a497551f55c
LinkyJax <Kaledo2805@gmail.com>
2017-12-31 17:58:56 +0300

    Решение задания №9

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -28,7 +28,7 @@ public class ContactModificationTests extends TestBase {
 
     before.remove(0);
     before.add(contact);
-    Comparator<? super ContactData> byId = (c1, c2) -> Integer.compare(c1.getId(), c2.getId());
+    Comparator<? super ContactData> byId =(c1,c2) -> Integer.compare(c1.getId(), c2.getId());
     before.sort(byId);
     after.sort(byId);
     Assert.assertEquals(before,after);
