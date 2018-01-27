24ffd3770a7ad60f634d181616e8981c19f0e0b2
LinkyJax <Kaledo2805@gmail.com>
2018-01-27 21:24:01 +0300

    Решение задания № 17

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -4,14 +4,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.opera.OperaDriver;
 import org.openqa.selenium.opera.OperaOptions;
 import org.openqa.selenium.remote.BrowserType;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
@@ -22,7 +20,12 @@ public class ApplicationManager {
   private final Properties properties;
   WebDriver wd;
 
+  private SessionHelper sessionHelper;
   private String browser;
+  private GroupHelper groupHelper;
+  private  NavigationHelper navigationHelper;
+  private ContactHelper contactHelper;
+  private DbHelper dbHelper;
 
   public ApplicationManager(String browser) {
     this.browser=browser;
@@ -32,6 +35,8 @@ public class ApplicationManager {
   public void init() throws IOException {
     String target=System.getProperty("target","local");
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties",target))));
+
+   dbHelper = new DbHelper();
 
     if (browser.equals(BrowserType.FIREFOX)) {
       wd=new FirefoxDriver(/*new FirefoxOptions().setLegacy(true)*/);
@@ -45,8 +50,12 @@ public class ApplicationManager {
       wd=new OperaDriver(options);}
       wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
       wd.get(properties.getProperty("web.baseUrl"));
+      groupHelper = new GroupHelper(wd);
+      contactHelper = new ContactHelper(wd);
+      navigationHelper = new NavigationHelper(wd);
+      sessionHelper = new SessionHelper(wd);
+      sessionHelper.login(properties.getProperty("web.adminLogin"),properties.getProperty("web.adminPassword"));
   }
-
 
   private void login(String username, String password) {
     wd.findElement(By.name("user")).click();
@@ -62,6 +71,12 @@ public class ApplicationManager {
     wd.quit();
   }
 
- 
+  public GroupHelper group() { return groupHelper; }
+
+  public NavigationHelper goTo() { return navigationHelper; }
+
+  public ContactHelper contact() { return contactHelper; }
+
+  public DbHelper db() {return dbHelper; }
 
 }
